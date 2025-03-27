95ea7835f7eb0b2feeade873a17bbd007221a263
petebrowncorndel <peter.brown@corndel.com>
2025-03-27 23:42:56 +0100

    solves maxOfThree

--- src/maxOfThree.js
+++ src/maxOfThree.js
@@ -2,13 +2,23 @@ function maxOfThree(x, y, z) {
   if (x > y && x > z) {
     return x
   }
-
-  if (y > z && y > x) {
+  else if (y > z && y > x) {
     return y
   }
-
-  if (z > x && z > y) {
+  else if (z > x && z > y) {
     return z
+  }
+  else if (x == y && x > z) {
+    return x
+  }
+  else if (x == z && x > y) {
+    return x
+  }
+  else if (y == z && y > x) {
+    return y
+  }
+  else if (y == z && y == x) {
+    return y
   }
 }
 
